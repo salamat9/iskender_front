96d1141ad1d3d1377aa87a34770bb12a59481797
Salamat Baichoroev <salamat@Salamats-MacBook-Pro.local>
2023-12-20 20:05:34 +0600

    ui fix

--- src/components/layouts/layout.jsx
+++ src/components/layouts/layout.jsx
@@ -4,11 +4,11 @@ import Footer from "./footer";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className="main-background">
       <Header />
       <div className="container">{children}</div>
       <Footer />
-    </>
+    </div>
   );
 };
 
