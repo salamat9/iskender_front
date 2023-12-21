571efa798c5fe915800cedba564657cf19af5994
Salamat Baichoroev <salamat@Salamats-MacBook-Pro.local>
2023-12-21 15:08:37 +0600

    fix

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL;
+const baseURL = 'https://app.moore22.store' || process.env.REACT_APP_API_URL;
 
 const api = axios.create({ baseURL });
 
